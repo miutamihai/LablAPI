6ab1067ea905ea31e39be2a86368e9006dc9f914
Mihai <miuta.mihai@gmail.com>
2020-02-25 13:44:43 +0000

    fixed unknown object bug

--- functions/get_max_probability.py
+++ functions/get_max_probability.py
@@ -3,6 +3,9 @@ import json
 
 def get_max_probability(load):
     result_list = list(json.loads(load)['result'])
-    res = result_list[0]['prediction'][0]
-    return res['probability'] > 0.5 and res['label'] or 'Unknown object'
+    res = result_list[0]['prediction']
+    number_of_results = len(res)
+    res = res[0]
+    threshold = number_of_results == 3 and 0.5 or 0.75
+    return res['probability'] > threshold and res['label'] or 'Unknown object'
 
